1b2051b960caff2fec44a485a3f34f34edbaabc8
Swagat Kumar Mishra ✦ <kumarmishraswagat415@gmail.com>
2025-07-24 01:02:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-// This line is essential. It imports the new Firebase messaging service worker.
-// It MUST be the very first line.
-importScripts('/firebase-messaging-sw.js');
-
 // This is the name for our cache. It's a good practice to include a version number.
-const CACHE_NAME = 'motive-hub-cache-v2'; // Note: I updated the version number to help force an update.
+const CACHE_NAME = 'motive-hub-cache-v1';
 
-// This is the list of files that we want to cache for offline use.
+// This is the list of files that we want to cache.
+// These are the essential files needed for your app to work offline.
 const urlsToCache = [
   '/',
   'index.html',
@@ -14,20 +11,24 @@ const urlsToCache = [
   'icon-512x512.png'
 ];
 
-// This event runs when the service worker is first installed.
+// This part of the code runs when the service worker is first installed.
 self.addEventListener('install', event => {
+  // We wait until the installation is complete.
   event.waitUntil(
+    // We open the cache by its name.
     caches.open(CACHE_NAME)
       .then(cache => {
+        // When the cache is open, we add all the files from our list to it.
         console.log('Opened cache and caching essential files');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// This event runs every time the app requests a file (like the HTML, an icon, etc.).
+// This part of the code runs every time the app requests a file (like the HTML, an icon, etc.).
 self.addEventListener('fetch', event => {
   event.respondWith(
+    // We check if the requested file is already in our cache.
     caches.match(event.request)
       .then(response => {
         // If we find a match in the cache, we return the cached version.
@@ -35,6 +36,7 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
+        
         // If the file is not in the cache, we fetch it from the internet as normal.
         return fetch(event.request);
       }
